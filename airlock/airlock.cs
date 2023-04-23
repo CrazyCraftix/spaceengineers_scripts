c7a472eb3bede8382af724c0fba2485833de5018
CrazyCraftix <43807375+CrazyCraftix@users.noreply.github.com>
2023-04-23 15:59:11 +0200

    airlock - improve readability

--- airlock/airlock.cs
+++ airlock/airlock.cs
@@ -4,23 +4,25 @@ private List<List<IMyDoor>> airlocks = new List<List<IMyDoor>>();
 public Program() {
 	// get blocks once -> recompile to update
 	GridTerminalSystem.GetBlockGroups(null, group => {
-		if (!group.Name.Contains(AIRLOCK_GROUP_IDENTIFIER)) return false;
-		airlocks.Add(new List<IMyDoor>());
-		group.GetBlocksOfType(airlocks.Last());
+		if (group.Name.Contains(AIRLOCK_GROUP_IDENTIFIER)) {
+			airlocks.Add(new List<IMyDoor>());
+			group.GetBlocksOfType(airlocks.Last());
+		}
 		return false;
 	});
 
 	Runtime.UpdateFrequency = UpdateFrequency.Update1;
 }
 
-public void Main(string argument, UpdateType updateType) {
+public void Main() {
 	// if one door opens in an airlock, disable all other doors
 	foreach (List<IMyDoor> airlock in airlocks) {
 		IMyDoor openDoor = null;
 		foreach (IMyDoor door in airlock) {
-			if (door.Status == DoorStatus.Closed) continue;
-			openDoor = door;
-			break;
+			if (door.Status != DoorStatus.Closed) {
+				openDoor = door;
+				break;
+			}
 		}
 		foreach (IMyDoor door in airlock) {
 			door.Enabled = (openDoor == null || openDoor == door);
