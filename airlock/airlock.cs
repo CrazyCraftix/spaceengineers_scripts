f0c701dfad101a2b3376018646509c2ecbf328ad
CrazyCraftix <43807375+CrazyCraftix@users.noreply.github.com>
2023-04-23 15:47:05 +0200

    airlock - get blocks inside constructor

--- airlock/airlock.cs
+++ airlock/airlock.cs
@@ -1,22 +1,19 @@
 private const string AIRLOCK_GROUP_IDENTIFIER = "<airlock>";
 
+private List<List<IMyDoor>> airlocks = new List<List<IMyDoor>>();
 public Program() {
+	// get blocks once -> recompile to update
+	GridTerminalSystem.GetBlockGroups(null, group => {
+		if (!group.Name.Contains(AIRLOCK_GROUP_IDENTIFIER)) return false;
+		airlocks.Add(new List<IMyDoor>());
+		group.GetBlocksOfType(airlocks.Last());
+		return false;
+	});
+
 	Runtime.UpdateFrequency = UpdateFrequency.Update1;
 }
 
-private List<List<IMyDoor>> airlocks = null;
 public void Main(string argument, UpdateType updateType) {
-	// get blocks once -> recompile to update
-	if (airlocks == null) {
-		airlocks = new List<List<IMyDoor>>();
-		GridTerminalSystem.GetBlockGroups(null, group => {
-			if (!group.Name.Contains(AIRLOCK_GROUP_IDENTIFIER)) return false;
-			airlocks.Add(new List<IMyDoor>());
-			group.GetBlocksOfType(airlocks.Last());
-			return false;
-		});
-	}
-
 	// if one door opens in an airlock, disable all other doors
 	foreach (List<IMyDoor> airlock in airlocks) {
 		IMyDoor openDoor = null;
